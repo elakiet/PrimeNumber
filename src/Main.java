9e20d80104754a5ba80fd75d20007a9b279c3f08
elakiet <elaki.kavin@gmail.com>
2023-08-31 10:37:29 +0530

    initial commit done

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number= scan.nextInt();
+        System.out.println(Prime.Findprime(number));
+        }
+    }
+
+class Prime
+{
+    static String Findprime(int number)
+    {
+        int temporary=0;
+        for(int iterator = 2 ; iterator <= number / 2 ; iterator++)
+        {
+            if(number % iterator == 0)
+            {
+                temporary=1;
+                break;
+            }
+        }
+        if (temporary != 1)
+            return "The number is a Prime";
+        else
+            return "The number is not Prime";
+    }
+}
